0e0e27c61733e092ddf3ccd558578bd8355d076a
bartosz.chyra <bartosz.chyra@rumblefish.pl>
2021-09-07 14:43:37 +0200

    wip

--- src/sagas/communities/launchCommunity/launchCommunity.saga.ts
+++ src/sagas/communities/launchCommunity/launchCommunity.saga.ts
@@ -4,5 +4,5 @@ import { identitySelectors } from "../../identity/identity.selectors";
 
 export function* launchCommunity(_action, socket): Generator {
     const selectedIdentity = yield* select(identitySelectors.selectById())
-    yield* apply(socket, socket.emit, [SocketActionTypes.LAUNCH_COMMUNITY, selectedIdentity.peerId, selectedIdentity.hiddenService.privateKey])
+    yield* apply(socket, socket.emit, [SocketActionTypes.LAUNCH_COMMUNITY, selectedIdentity.peerId, selectedIdentity.hiddenService])
 }
---
+++ src/sagas/communities/launchRegistrar/launchRegistrar.saga.ts
@@ -0,0 +1,8 @@
+import { apply, select } from "typed-redux-saga";
+import { SocketActionTypes } from "../../socket/const/actionTypes";
+import { identitySelectors } from "../../identity/identity.selectors";
+
+export function* launchRegistrar(_action, socket): Generator {
+    const selectedIdentity = yield* select(identitySelectors.selectById())
+    yield* apply(socket, socket.emit, [SocketActionTypes.LAUNCH_COMMUNITY, selectedIdentity.peerId, selectedIdentity.hiddenService])
+}
