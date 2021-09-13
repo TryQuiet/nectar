afaaca3e69e4d853f31ad3f2b24869717fdaa3e0
Emilia Marczyk <emilia.marczyk@rumblefish.pl>
2021-09-13 18:41:46 +0200

    Cleanup the integration test

--- src/tests/integration.ts
+++ src/tests/integration.ts
@@ -28,19 +28,19 @@ function* createCommunityTestSaga(payload): Generator {
   yield put(communitiesActions.createNewCommunity(communityName))
   yield take(communitiesActions.responseCreateCommunity)
   const currentCommunity = yield* select(communitiesSelectors.currentCommunity())
-  console.log('CURRENT', currentCommunity)
   assert.equal(currentCommunity.name, communityName)
   assert(currentCommunity.onionAddress)
   assert(currentCommunity.port)
   assert(currentCommunity.privateKey)
   yield put(identityActions.registerUsername(userName))
   yield take(identityActions.storeUserCertificate)
+  // TODO: how to check for registration (and other) errors?
   // const certificateRegistrationError = yield* select(errorsSelectors.certificateRegistration)
   // console.log(certificateRegistrationError)
   // assert.equal(certificateRegistrationError, undefined, `Registration failed: ${certificateRegistrationError}`)
   // check if we have cert and cert ca
   const createdIdentity = yield* select(identitySelectors.currentIdentity)
-  console.log('IDENTITY', createdIdentity)
+  console.log(`User ${userName} identity:`, createdIdentity)
   assert.equal(createdIdentity.id, currentCommunity.id)
   assert.notEqual(createdIdentity.peerId, undefined)
   assert.notEqual(createdIdentity.userCertificate, undefined)
@@ -69,30 +69,41 @@ function* joinCommunityTestSaga(payload): Generator {
   assert.notEqual(createdIdentity.peerId, undefined)
   assert.notEqual(createdIdentity.userCertificate, undefined)
   assert.notEqual(createdIdentity.hiddenService, undefined)
-  
+}
+
+const isOwnerAppReady = (ownerAppState): boolean => {
+  // TODO: can I use selectors outside sagas and react components?
+  const communitiesState = ownerAppState.Communities
+  const identityState = ownerAppState.Identity
+  const useridentity = identityState.entities[identityState.ids[0]]
+  const communities = communitiesState.communities
+  const mainCommunityId = communitiesState.currentCommunity
+  return mainCommunityId && communities.entities[mainCommunityId].onionAddress && useridentity && useridentity.userCertificate
 }
 
 const main = async () => {
   const store1 = await createApp('First', handleTestActions)
   const store2 = await createApp('Second', handleTestActions)
 
+  // Owner creates community and registers
   store1.dispatch({ type: 'userCreatingCommunity', payload: { userName: 'Owner' } })
 
   const unsubscribe = store1.subscribe(async () => {
-    const communitiesState = store1.getState().Communities
-    const identityState = store1.getState().Identity
-    const useridentity = identityState.entities[identityState.ids[0]]
-    const communities = communitiesState.communities
-    const mainCommunityId = communitiesState.currentCommunity
+    const ownerStoreState = store1.getState()
 
-    // TODO: simplify this ^
-    if (mainCommunityId && communities.entities[mainCommunityId].onionAddress && useridentity && useridentity.userCertificate) {
+    // User joins community and registers as soon as the owner finishes registering
+    if (isOwnerAppReady(ownerStoreState)) {
       unsubscribe()
-      const community = communities.entities[mainCommunityId]
+      const community = ownerStoreState.Communities.communities.entities[ownerStoreState.Communities.currentCommunity]
       const registrarAddress = `http://${community.onionAddress}.onion:${community.port}`
+      const ownerIdentityState = store1.getState().Identity
       store2.dispatch({ 
         type: 'userJoiningCommunity', 
-        payload: { userName: 'User', registrarAddress, communityId: community.id, ownerPeerId: useridentity.peerId.id }
+        payload: { 
+          userName: 'User', 
+          registrarAddress, communityId: community.id, 
+          ownerPeerId: ownerIdentityState.entities[ownerIdentityState.ids[0]].peerId.id 
+        }
       })
     }
   })
