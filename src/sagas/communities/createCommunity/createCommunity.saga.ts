185796c0c9ef7d7c709f55b33e66b8674f532317
bartosz.chyra <bartosz.chyra@rumblefish.pl>
2021-09-21 17:58:11 +0200

    new flow wkorking

--- src/sagas/communities/createCommunity/createCommunity.saga.ts
+++ src/sagas/communities/createCommunity/createCommunity.saga.ts
@@ -1,5 +1,6 @@
 import {createRootCA} from '@zbayapp/identity'
-import Time from 'pkijs/src/Time'
+// @ts-ignore
+import { Time } from 'pkijs'
 import { call, apply, put } from 'typed-redux-saga'
 import { communitiesActions } from '../communities.slice'
 import { SocketActionTypes } from "../../socket/const/actionTypes";
@@ -13,7 +14,6 @@ const notAfterDate = new Date(Date.UTC(2030, 11, 28, 10, 10, 10))
 const rootCa = yield* call(createRootCA, new Time({ type: 0, value: notBeforeDate }), new Time({ type: 0, value: notAfterDate }) )
 const id = yield* call(generateId)
 const payload = {id: id, CA: rootCa, name: action.payload, registrarUrl: '' }
-console.log(rootCa, 'jebanerootca')
 yield* put(communitiesActions.addNewCommunity(payload))
 yield* put(communitiesActions.setCurrentCommunity(id))
 // yield* put(publicChannelsActions.addPublicChannelsList({id}))
