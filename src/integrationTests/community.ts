05a1c2926107071737e2a16d723d174a1b3909e2
Emilia Marczyk <emilia.marczyk@rumblefish.pl>
2021-09-29 18:42:17 +0200

    Add third user to integration test

--- src/integrationTests/community.ts
+++ src/integrationTests/community.ts
@@ -38,7 +38,8 @@ function* createCommunityTestSaga(payload): Generator {
 }
 
 function* joinCommunityTestSaga(payload): Generator {
-  const { registrarAddress, userName, ownerPeerId, ownerRootCA } = payload
+  const { registrarAddress, userName, ownerPeerId, ownerRootCA, expectedPeersCount } = payload
+  console.log('USER::::', userName)
   yield* fork(assertNoErrors)
   yield* put(communitiesActions.joinCommunity(registrarAddress))
   yield* take(communitiesActions.responseCreateCommunity)
@@ -49,7 +50,7 @@ function* joinCommunityTestSaga(payload): Generator {
   const createdIdentity = yield* select(identitySelectors.currentIdentity)
   assert.equal(currentCommunity.rootCa, ownerRootCA, "User joining community should have the same rootCA as the owner")
   assert.notEqual(currentCommunity.peerList, undefined, "User joining community should have a list of peers to connect to")
-  assert.equal(currentCommunity.peerList.length, 2, "User joining community should receive a full list of peers to connect to")
+  assert.equal(currentCommunity.peerList.length, expectedPeersCount, `User joining community should receive a full list of peers to connect to. Received ${currentCommunity.peerList.length}, expected ${expectedPeersCount}`)
   assertListElementMatches(currentCommunity.peerList, new RegExp(ownerPeerId))
   assertListElementMatches(currentCommunity.peerList, new RegExp(createdIdentity.peerId.id))
   assert.equal(createdIdentity.zbayNickname, userName)
@@ -57,32 +58,63 @@ function* joinCommunityTestSaga(payload): Generator {
   assertNotEmpty(createdIdentity.peerId, 'Identity.peerId')
   assertNotEmpty(createdIdentity.userCertificate, 'Identity.userCertificate')
   assertNotEmpty(createdIdentity.hiddenService, 'Identity.hiddenService')
+  yield* put(createAction('testContinue')())
+}
+
+function* finishTestSaga () {
   yield* put(createAction('testFinished')())
 }
 
 const testUsersCreateAndJoinCommunitySuccessfully = async () => {
   const app1 = await createApp()
   const app2 = await createApp()
-  watchResults([app1, app2], app2, 'Users create and join community successfully')
+  const app3 = await createApp()
+  watchResults([app1, app2, app3], app3, 'Users create and join community successfully')
 
   // Owner creates community and registers
   app1.runSaga(integrationTest, createCommunityTestSaga, { userName: 'Owner' })
 
-  const unsubscribe = app1.store.subscribe(async () => {
+  const unsubscribeApp2 = app2.store.subscribe(() => {
+    if (app2.store.getState().Test.continue) {
+      unsubscribeApp2()
+      const ownerStoreState = app1.store.getState()
+      const community = ownerStoreState.Communities.communities.entities[ownerStoreState.Communities.currentCommunity]
+      const registrarAddress = `http://${community.onionAddress}:${community.port}`
+      const ownerIdentityState = app1.store.getState().Identity
+      app3.runSaga(integrationTest, joinCommunityTestSaga, {
+        userName: 'User2', 
+        registrarAddress, 
+        communityId: community.id, 
+        ownerPeerId: ownerIdentityState.entities[ownerIdentityState.ids[0]].peerId.id,
+        ownerRootCA: community.rootCa,
+        expectedPeersCount: 3
+      })
+    }
+  })
+
+  const unsubscribeApp1 = app1.store.subscribe(async () => {
     // User joins community and registers as soon as the owner finishes registering
     if (app1.store.getState().Test.continue) {
-      unsubscribe()
+      unsubscribeApp1()
       const ownerStoreState = app1.store.getState()
       const community = ownerStoreState.Communities.communities.entities[ownerStoreState.Communities.currentCommunity]
       const registrarAddress = `http://${community.onionAddress}:${community.port}`
       const ownerIdentityState = app1.store.getState().Identity
       app2.runSaga(integrationTest, joinCommunityTestSaga, {
-        userName: 'User', 
+        userName: 'User1', 
         registrarAddress, 
         communityId: community.id, 
         ownerPeerId: ownerIdentityState.entities[ownerIdentityState.ids[0]].peerId.id,
-        ownerRootCA: community.rootCa
+        ownerRootCA: community.rootCa,
+        expectedPeersCount: 2
       })
+    }
+  })
+
+  const unsubscribeApp3 = app3.store.subscribe(() => {
+    if (app3.store.getState().Test.continue) {
+      unsubscribeApp3()
+      app3.runSaga(finishTestSaga)
     }
   })
 }
