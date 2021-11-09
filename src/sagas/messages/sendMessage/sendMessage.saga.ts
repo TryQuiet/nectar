53be4ebdc4e0b61925abc8bd3919425d3a110832
siepra <wiktor.siepra@gmail.com>
2021-11-09 18:04:59 +0100

    Fixed sendMessageSaga and publicChannels reducer

--- src/sagas/messages/sendMessage/sendMessage.saga.ts
+++ src/sagas/messages/sendMessage/sendMessage.saga.ts
@@ -5,25 +5,19 @@ import {
   parseCertificate,
   sign,
   loadPrivateKey,
-  extractPubKeyString,
 } from '@zbayapp/identity/lib';
-
-import { config } from '../../users/const/certFieldTypes';
-
 import { call, select, apply } from 'typed-redux-saga';
 import { arrayBufferToString } from 'pvutils';
+import { config } from '../../users/const/certFieldTypes';
 import { SocketActionTypes } from '../../socket/const/actionTypes';
 import { identitySelectors } from '../../identity/identity.selectors';
 import { publicChannelsSelectors } from '../../publicChannels/publicChannels.selectors';
 import { messagesActions } from '../messages.slice';
 import { MessageTypes } from '../const/messageTypes';
-// import { navigateTo } from '../../../utils/functions/navigateTo/navigateTo';
-// import { ScreenNames } from '../../../const/ScreenNames.enum';
-// import { Dispatch } from 'react';
-// import { appImages } from '../../../../assets';
-// import { replaceScreen } from '../../../utils/functions/replaceScreen/replaceScreen';
 import { generateMessageId, getCurrentTime } from '../utils/message.utils';
 import logger from '../../../utils/logger'
+import { Identity } from 'src/sagas/identity/identity.slice';
+
 const log = logger('message')
 
 export function* sendMessageSaga(
@@ -32,8 +26,10 @@ export function* sendMessageSaga(
     ReturnType<typeof messagesActions.sendMessage>['payload']
   >
 ): Generator {
-  const identity = yield* select(identitySelectors.currentIdentity);
-  if (!identity.userCertificate) {
+  const identity: Identity = yield* select(identitySelectors.currentIdentity);
+  
+  const certificate = identity.userCertificate;
+  if (!certificate) {
     // TODO
     // yield* call(navigateTo, ScreenNames.ErrorScreen, {
     //   onPress: (_dispatch: Dispatch<any>) => {
@@ -46,25 +42,7 @@ export function* sendMessageSaga(
     // });
     return;
   }
-  log(identity.userCsr.pkcs10.privateKey, 'KeyObject');
-
   log('sendMessageSaga-1');
-
-  const certificate = null;
-  // const certificate = yield* select(identitySelectors.userCertificate);
-  if (!certificate) {
-    // TODO
-    // yield* call(navigateTo, ScreenNames.ErrorScreen, {
-    //   onPress: (_dispatch: Dispatch<any>) => {
-    //     replaceScreen(ScreenNames.MainScreen);
-    //   },
-    //   icon: appImages.zbay_icon,
-    //   title: 'Error',
-    //   message:
-    //     'User certificate is missing. You need it to let the others know which messages was sent by you. Try to register your username again.',
-    // });
-    return;
-  }
 
   const parsedCertificate = yield* call(parseCertificate, certificate);
   const pubKey = yield* call(keyFromCertificate, parsedCertificate);
@@ -76,10 +54,9 @@ export function* sendMessageSaga(
   const signatureArrayBuffer = yield* call(sign, action.payload, keyObject);
   const signature = yield* call(arrayBufferToString, signatureArrayBuffer);
 
-  const channel = yield* select(publicChannelsSelectors.currentChannel);
+  const channelAddress = yield* select(publicChannelsSelectors.currentChannel);
 
   const messageId = yield* call(generateMessageId);
-
   const currentTime = yield* call(getCurrentTime);
 
   const message = {
@@ -89,14 +66,13 @@ export function* sendMessageSaga(
     createdAt: currentTime,
     signature,
     pubKey,
-    channelId: channel,
+    channelId: channelAddress,
   };
-
   yield* apply(socket, socket.emit, [
     SocketActionTypes.SEND_MESSAGE,
+    identity.peerId.id,
     {
-      communityId: identity.id,
-      channelAddress: channel,
+      channelAddress: channelAddress,
       message,
     },
   ]);
